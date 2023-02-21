cdb4d2e3833bc19bf1c0ff348cc8b4455ab02d1f
Cristian Vargas <cristian.v0223@gmail.com>
2023-02-21 17:51:49 -0600

    added Confirmation component to checkout page directory for the success checkout

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import {
   useLocation,
 } from "react-router-dom"
 import Checkout from './scenes/checkout/Checkout';
+import Confirmation from './scenes/checkout/Confirmation';
 import Home from "./scenes/home/Home"
 import ItemDetails from './scenes/itemDetails/ItemDetails';
 
@@ -30,6 +31,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/item/:itemId" element={<ItemDetails />} />
           <Route path="/checkout" element={<Checkout />} />
+          <Route path="/checkout/success" element={<Confirmation />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ client/src/scenes/checkout/Confirmation.jsx
@@ -0,0 +1,9 @@
+// Component that will set up our layout on our Confirmation page.
+
+const Confirmation = () => {
+    return (
+      <div>Confirmation</div>
+    )
+  }
+  
+export default Confirmation
