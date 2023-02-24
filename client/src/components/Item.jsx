e2b034679a0d393a3460af62584f7142bbb3c36b
Cristian Vargas <cristian.v0223@gmail.com>
2023-02-24 13:04:35 -0600

    added additional content to item component

--- client/src/components/Item.jsx
+++ client/src/components/Item.jsx
@@ -53,27 +53,47 @@ const Item = ({ item, width}) => {
                 padding="0 5%"
                 >
                     <Box display="flex" justifyContent="space-between">
-                        
+
                       {/* Amount Here */}
                       <Box display="flex" alignItems="center" backgroundColor={shades.neutral[100]} borderRadius="3px">
-                        <IconButton
-                            onClick={() =>
-                                setCount(Math.max(count -1, 1))
-                            }
+                            <IconButton
+                                onClick={() =>
+                                    setCount(Math.max(count -1, 1))
+                                }
                             >
                             <RemoveIcon />
                             </IconButton>
                             <Typography color={shades.primary[300]}>{count}</Typography>
                             <IconButton
-                            onClick={() =>
-                                setCount(count +1)
-                            }
+                                onClick={() =>
+                                    setCount(count +1)
+                                }
                             >
                             <AddIcon />
                             </IconButton>
                       </Box>
+
+                      {/* Button Here */}
+                      <Button 
+                        onClick={() => { dispatch(addToCart({ item: {...item, count}}))}}
+                        sx={{
+                            backgroundColor: shades.primary[300], color: "white"
+                        }}
+                      >
+                        Add to Cart
+                      </Button>
                     </Box>
                 </Box>
+            </Box>
+            <Box mt="3px">
+              <Typography variant='subtitle2' color={neutral.dark}>
+                {category
+                  .replace(/([A-Z])/g, "$1")
+                  .replace(/^./, (str) => str.toUpperCase())
+                }
+              </Typography>
+              <Typography>{name}</Typography>
+              <Typography fontWeight="bold">{price}</Typography>
             </Box>
         </Box>
     )
