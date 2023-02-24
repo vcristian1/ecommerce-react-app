83922daf83be4f91f0970dfe332e3f76736c3d6e
Cristian Vargas <cristian.v0223@gmail.com>
2023-02-24 12:55:18 -0600

    created item component in component directory this will be the card showing each item for sale and will be reused across multiple pages

---
+++ client/src/components/Item.jsx
@@ -0,0 +1,82 @@
+//Item component is the actual item card, and is included in components since it is used in multiple scenes.
+import { useState } from 'react';
+import { useDispatch } from 'react-redux'
+import { IconButton, Box, Typography, useTheme, Button} from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+import { shades } from '../theme'
+import { addToCart} from '../state'
+import { useNavigate } from 'react-router-dom';
+import { borderRadius } from '@mui/system';
+
+const Item = ({ item, width}) => {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const [count, setCount] = useState(1);
+    const [isHovered, setIsHovered] = useState(false);
+    const { palette: { neutral },
+    } = useTheme();
+
+    const { category, price, name, image } = image.attributes;
+    // Destructuring the image data
+    const {
+        data: {
+            attributes: {
+                formats: {
+                    medium: { url },
+                }
+            }
+        }
+    } = image;
+
+    return (
+        <Box width={width}>
+            <Box 
+             position="relative" 
+             onMouseOver={() => setIsHovered(true)}
+             onMouseOut={() => setIsHovered(false)}
+             >
+                <img 
+                src={`http://localhost:1337${url}`}
+                alt={item.name} 
+                height="400px"
+                width="300px"
+                onClick={() => navigate(`/item/${item.id}`)}
+                style={{ cursor: 'pointer'}}
+                />
+                <Box
+                display={isHovered ? "blocked" : "none"}
+                position="absolute"
+                bottom="10%"
+                left="0"
+                width="100%"
+                padding="0 5%"
+                >
+                    <Box display="flex" justifyContent="space-between">
+                        
+                      {/* Amount Here */}
+                      <Box display="flex" alignItems="center" backgroundColor={shades.neutral[100]} borderRadius="3px">
+                        <IconButton
+                            onClick={() =>
+                                setCount(Math.max(count -1, 1))
+                            }
+                            >
+                            <RemoveIcon />
+                            </IconButton>
+                            <Typography color={shades.primary[300]}>{count}</Typography>
+                            <IconButton
+                            onClick={() =>
+                                setCount(count +1)
+                            }
+                            >
+                            <AddIcon />
+                            </IconButton>
+                      </Box>
+                    </Box>
+                </Box>
+            </Box>
+        </Box>
+    )
+}
+
+export default Item
