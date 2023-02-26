6ee7be62067af4efe515b965fce7abdbeef0777f
Cristian Vargas <cristian.v0223@gmail.com>
2023-02-26 07:40:50 -0600

    resolved fetch error, items are being logged to console needed to adjust local host link

--- client/src/scenes/home/Home.jsx
+++ client/src/scenes/home/Home.jsx
@@ -1,10 +1,12 @@
 // Component that will set up our layout on our homepage.
 import MainCarousel from './MainCarousel';
+import ShoppingList from './ShoppingList'
 
 const Home = () => {
   return (
     <div className='home'>
       <MainCarousel />
+      <ShoppingList />
     </div>
   )
 }
---
+++ client/src/scenes/home/ShoppingList.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Box from "@mui/material/Box";
+import Item from "../../components/Item";
+import { Typography } from "@mui/material";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useDispatch, useSelector } from "react-redux";
+import { setItems } from "../../state";
+
+const ShoppingList = () => {
+  const dispatch = useDispatch();
+  const [value, setValue] = useState("all");
+  const items = useSelector((state) => state.cart.items);
+  const breakPoint = useMediaQuery("(min-width:600px)");
+  console.log("items", items);
+  
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  async function getItems() {
+    const items = await fetch(
+      "http://localhost:1337/api/items?populate=image",
+      { method: "GET" }
+    );
+    const itemsJson = await items.json();
+    dispatch(setItems(itemsJson.data));
+  }
+
+  useEffect(() => {
+    getItems();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  return (
+    <div>Shopping List</div>
+  );
+};
+
+export default ShoppingList;
