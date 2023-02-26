8d354f2f18e31373602376b2fcdaa206807c7c5b
Cristian Vargas <cristian.v0223@gmail.com>
2023-02-26 10:10:11 -0600

    added IconButton and Remove Icon so you can add the item to the cart

--- client/src/scenes/itemDetails/ItemDetails.jsx
+++ client/src/scenes/itemDetails/ItemDetails.jsx
@@ -11,8 +11,93 @@ import Item from '../../components/Item';
 
 // Component that will set up our layout on our Item Details page.
 const ItemDetails = () => {
-    return (
-      <div>ItemDetails</div>
+  const dispatch = useDispatch();
+  const { itemId } = useParams();
+  const [value, setValue] = useState("description");
+  const [count, setCount] = useState(1);
+  const [item, setItem] = useState(null);
+  const [items, setItems] = useState([]);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  }
+
+  async function getItem() {
+    const item = await fetch(
+      `http://localhost:1337/api/items/${item.id}?populate=image`,
+      { method: "GET" }
+    );
+    const itemJson = await item.json();
+    setItem(itemJson.data);
+  }
+
+  async function getItems() {
+    const items = await fetch(
+      "http://localhost:1337/api/items?populate=image",
+      { method: "GET" }
+    );
+    const itemsJson = await items.json();
+    setItems(itemsJson.data);
+  }
+
+  useEffect(() => {
+    getItem();
+    getItems();
+  }, [itemId])
+    
+  return (
+      <Box width="80%" m="80px auto">
+        <Box
+        display="flex"
+        flexWrap="wrap"
+        columnGap="40px"
+        >
+          {/* Images Here */}
+          <Box flex="1 1 40%" mb="40px">
+          <img
+            alt={item?.name}
+            width="100%"
+            height="100%"
+            src={`http://localhost:1337${item?.attributes?.image?.data?.attributes?.formats?.medium?.url}`}
+            style={{ objectFit: "contain" }}
+          />
+          </Box>
+          {/* Actions Here */}
+          <Box flex="1 1 50%" mb="40px">
+            <Box display="flex" justifyContent="space-between">
+              <Box>Home/Item</Box>
+              <Box>Previous Next</Box>
+            </Box>
+
+            <Box m="65px 0 25px 0">
+              <Typography variant='h3'>{item?.attributes?.name}</Typography>
+              <Typography>${item?.attributes?.price}</Typography>
+              <Typography sx={{ mt: "20px" }}>
+                {item?.attributes?.longDescription}
+              </Typography>
+            </Box>
+            <Box display="flex" alignItems="center" minHeight="50px">
+              <Box display="flex" alignItems="center" border={`1.5px solid ${shades.neutral[300]}`} mr="20px" p="2px 5px">
+                <IconButton
+                  onClick={() =>
+                  setCount(Math.max(count -1, 1))
+                }
+                >
+                <RemoveIcon />
+                </IconButton>
+                <Typography sx={{ p: "0 5px"}}>{count}</Typography>
+                <IconButton
+                  onClick={() =>
+                  setCount(count +1)
+                }
+                >
+                <AddIcon />
+                </IconButton>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
     )
   }
   
