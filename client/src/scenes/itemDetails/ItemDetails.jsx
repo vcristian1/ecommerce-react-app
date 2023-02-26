99671448c29699a6a901f7e21bf43d9cae887ff3
Cristian Vargas <cristian.v0223@gmail.com>
2023-02-26 09:38:00 -0600

    added neccessary imports to build out ItemDetails page

--- client/src/scenes/itemDetails/ItemDetails.jsx
+++ client/src/scenes/itemDetails/ItemDetails.jsx
@@ -1,3 +1,14 @@
+import { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux'
+import { IconButton, Box, Typography, Button, Tabs, Tab} from '@mui/material';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+import { shades } from '../../theme'
+import { addToCart} from '../../state'
+import { useParams } from 'react-router-dom';
+import Item from '../../components/Item';
+
 // Component that will set up our layout on our Item Details page.
 const ItemDetails = () => {
     return (
