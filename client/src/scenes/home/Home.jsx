3f63e60ca242fc37941b601e904aa84c96222f66
Cristian Vargas <cristian.v0223@gmail.com>
2023-02-26 08:52:06 -0600

    created Subscribe component and added to Home page

--- client/src/scenes/home/Home.jsx
+++ client/src/scenes/home/Home.jsx
@@ -1,14 +1,16 @@
 // Component that will set up our layout on our homepage.
 import MainCarousel from './MainCarousel';
 import ShoppingList from './ShoppingList'
+import Subscribe from './Subscribe';
 
 const Home = () => {
   return (
     <div className='home'>
       <MainCarousel />
       <ShoppingList />
+      <Subscribe />
     </div>
   )
 }
 
-export default Home
+export default Home;
