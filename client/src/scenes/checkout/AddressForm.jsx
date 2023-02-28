0eaae49a61e8efeb578dadd9e4f4293676c5d2ef
Cristian Vargas <cristian.v0223@gmail.com>
2023-02-28 15:26:02 -0600

    added imports to addressForm component which is shown in checkoutPage and also included functions to format the name and error in AddressForm

--- client/src/scenes/checkout/AddressForm.jsx
+++ client/src/scenes/checkout/AddressForm.jsx
@@ -1,6 +1,30 @@
-import React from 'react'
+import { useMediaQuery, TextField } from "@mui/material";
+import { getIn } from "formik";
 
-export const AddressForm = () => {
+
+export const AddressForm = ({
+    type,
+    value,
+    errors,
+    touched,
+    handleBlur,
+    handleChange,
+}) => {
+  const isNonMobile = useMediaQuery("(min-width:600px)");
+
+  // These functions allow for better code readbaility.
+
+  // This allows us to format the name easily.
+  const formattedName = (field) => `${type}.${field}`;
+
+  // This allows us to format errors easily.
+  const formattedError = (field) => 
+    Boolean(
+        // Formik function that checks first the touched object first to see if it is true or false. Then does the same with the errors object, and if either one is false it wont show an error both need to be true. 
+        // We use getIn so when we are referencing something via formattedname we have to use getIn() or it wont be able to grab the field.
+        getIn(touched, formattedName(field)) &&
+        getIn(errors, formattedName(field))
+    )
   return (
     <div>AddressForm</div>
   )
