7a64476f8368fabf7fd2b19431e4c4f8eb9c0578
Cristian Vargas <cristian.v0223@gmail.com>
2023-03-02 20:41:27 -0600

    wrote logic in order.js to create a stripe session, create the item in the backend,return the session id and any errors

--- server/src/api/order/controllers/order.js
+++ server/src/api/order/controllers/order.js
@@ -21,6 +21,8 @@ module.exports = createCoreController('api::order.order', ({ strapi }) => ({
                     .findOne(prodct.id);
                     
                     return {
+                    // Structure comes from Stripe documentation. Prebuilt Checkout
+                    // lineItems represents the list of objects created below
                       price_data: {
                         currency: "usd",
                         product_data: {
@@ -31,9 +33,28 @@ module.exports = createCoreController('api::order.order', ({ strapi }) => ({
                       quantity: product.count,
                   }
                 })
-            )
-        } catch (error) {
+            );
+            // create stripe session
+            const session = await stripe.checkout.sessions.create({
+                payment_method_types: ["card"],
+                customer_email: email,
+                mode: "payment",
+                success_url: "http://localhost:3000/checkout/success",
+                cancel_url: "http:locahost:3000",
+                // lineItems represents the list of objects created below
+                line_items: lineItems
+            });
 
+            // create the item in our backend automatically
+            await strapi.service("api::order.order").create({
+                data: { userName, products, stripeSessionId: session.id },
+            });
+
+            // return the session id
+            return { id: session.id } 
+        }  catch (error) {
+            ctx.response.status = 500;
+            return { error: { message: "There was a problem creating the charge." } };
         }
-    }
+    },
 }));
