5111d977f111579185ec3284714f809584aff02e
Cristian Vargas <cristian.v0223@gmail.com>
2023-03-02 20:30:59 -0600

    downloaded stripe dependency to server directory, and configured stripe in order.js

--- server/src/api/order/controllers/order.js
+++ server/src/api/order/controllers/order.js
@@ -1,4 +1,5 @@
 'use strict';
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 /**
  * order controller
@@ -6,4 +7,33 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::order.order');
+module.exports = createCoreController('api::order.order', ({ strapi }) => ({
+    async create(ctx) {
+        const { products, userName, email } = ctx.request.body
+
+        try {
+            //retrieve item info first
+            const lineItems = await Promise.all(
+                produce.map(async (product) => {
+                // Grabbing the item then returning an object that has price and quantity data
+                  const item = await strapi
+                    .service("api::item.item")
+                    .findOne(prodct.id);
+                    
+                    return {
+                      price_data: {
+                        currency: "usd",
+                        product_data: {
+                            name: item.name
+                        },
+                        unit_amount: item.price * 100
+                      },
+                      quantity: product.count,
+                  }
+                })
+            )
+        } catch (error) {
+
+        }
+    }
+}));
