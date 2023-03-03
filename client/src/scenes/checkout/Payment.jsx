2471df06110a36631688cfcb3025b0434919f638
Cristian Vargas <cristian.v0223@gmail.com>
2023-03-02 18:20:58 -0600

    added payment component content

--- client/src/scenes/checkout/Payment.jsx
+++ client/src/scenes/checkout/Payment.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import { Box, Typography, TextField } from "@mui/material";
 
-export const Payment = () => {
+export const Payment = ({ values, touched, errors, handleBlur, handleChange}) => {
   return (
-    <div>Payment</div>
+    <Box m="30px 0">
+        {/* Contact Info Here */}
+        <Typography sx={{ mb: "15px" }} fontSize="18px"></Typography>
+        <TextField 
+         fullWidth
+         type="text"
+         label="Email"
+         onBlur={handleBlur}
+         onChange={handleChange}
+         value={values.email}
+         name="email"
+         // Converts email to a boolean
+         error={!!touched.email && !!errors.email}
+         helperText={touched.email && errors.email}
+         sx={{ gridColumn: "span 4", marginBottom: "15px" }}
+        />
+        <TextField 
+         fullWidth
+         type="text"
+         label="Phone Number"
+         onBlur={handleBlur}
+         onChange={handleChange}
+         value={values.phoneNumber}
+         name="phoneNumber"
+         // Converts email to a boolean
+         error={!!touched.phoneNumber && !!errors.phoneNumber}
+         helperText={touched.phoneNumber && errors.phoneNumber}
+         sx={{ gridColumn: "span 4" }}
+        />
+    </Box>
   )
 }
 
