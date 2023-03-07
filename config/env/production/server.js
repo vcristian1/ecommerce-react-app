9ed8130bf85be199a277169c60cd770b7dbdfbd7
Cristian Vargas <cristian.v0223@gmail.com>
2023-03-07 08:39:42 -0600

    deploying...

--- config/env/production/server.js
+++
@@ -1,9 +0,0 @@
-// Path: ./config/env/production/server.js
-
-module.exports = ({ env }) => ({
-    proxy: true,
-    url: env('MY_HEROKU_URL'), // Sets the public URL of the application.
-    app: { 
-      keys: env.array('APP_KEYS')
-    },
-});
